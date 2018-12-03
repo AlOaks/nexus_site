0a240388f019488f58922d694ac09b1f82a99b1f
Rafanexus <jalberto.robles@outlook.com>
2018-12-03 13:16:45 -0800

    Files files

--- themes/nexus_theme_play/taxonomy-programstypes-diploma.php
+++ themes/nexus_theme_play/taxonomy-programstypes-diploma.php
@@ -13,7 +13,7 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div class="page-title-container">
-				<h1><?php _e('Diplomas', 'nexus'); ?></h1>
+				<h1><?php _e('Diploma', 'nexus'); ?></h1>
 				<p><?php _e(term_description(), 'nexus'); ?></p>
 			</div>
 			<div class="filters-container">
