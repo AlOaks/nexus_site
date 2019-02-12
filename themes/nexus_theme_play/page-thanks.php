76480c813000fa82df12108d826f222b6844d5d4
Rafanexus <jalberto.robles@outlook.com>
2019-02-12 14:49:19 -0800

    Thank you Page

--- themes/nexus_theme_play/page-thanks.php
+++ themes/nexus_theme_play/page-thanks.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 
+ * Template Name: Thanks
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -18,17 +18,11 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div class="page-title-container">
-				<h1 class="type-page-title"><?php _e(the_title(), 'nexus'); ?></h1>
 			</div>
-			<div class="page-form">
-		<?php while ( have_posts() ) : the_post(); ?>
-			
-
-		 	<?php the_content(); ?>
-			
-		<?php
-		endwhile; // End of the loop.
-		?>
+			<div class="thanks-page-form">
+                <h2><? _e('Thank You!', 'nexus'); ?></h2>
+                <p><? _e('We will get back to you as soon as possible!', 'nexus'); ?></p>
+                <a href="https://nexuseducanada.com"><? _e('Back to home', 'nexus'); ?></a>
 			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
