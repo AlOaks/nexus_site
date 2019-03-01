958a5e2463cfe3df2a847790c4a83ab3c6aca832
Al Oaks <jalberto.robles@outlook.com>
2019-03-01 11:35:15 -0800

    Promotions

--- themes/nexus_theme_play/page-promotions.php
+++ themes/nexus_theme_play/page-promotions.php
@@ -23,14 +23,12 @@ get_header();
                     <?
                         $args = array(
                             'post_type' => 'schools',
-                            'order' => 'ASC',
-                            'tag' => 'private',
-                            'posts_per_page' => 1
+                            'order' => 'ASC'
                         ); 
 
-                        $privateSchools = new WP_query($args);
+                        $promoSchools = new WP_query($args);
 
-                        while($privateSchools->have_posts() ) : $privateSchools->the_post();
+                        while($promoSchools->have_posts() ) : $promoSchools->the_post();
                     
                             $promo = CFS()->get('on_promo');                    
                             $school_type = get_the_tags();
@@ -41,7 +39,7 @@ get_header();
                         if($promo == true) { ?>
                             
                             <div class="school-div" style="background: url(<? echo get_the_post_thumbnail_url(); ?>) no-repeat center;">
-                                <div class="school-ovrly private"></div>
+                                <div class="school-ovrly"></div>
                                 <h2 class="school-title title"><? the_title(); ?></h2>
                                 <p class="school-type"><? echo $schoolType; ?></p>
                                 <div class="school-content">
@@ -51,77 +49,8 @@ get_header();
                             </div>
 
                         <? } ?>
-                    <? endwhile; ?>
-                    <?
-                        $args = array(
-                            'post_type' => 'schools',
-                            'posts_per_page' => 1,
-                            'order' => 'ASC',
-                            'tag' => 'public'
-                        ); 
-
-                        $privateSchools = new WP_query($args);
-
-                        while($privateSchools->have_posts() ) : $privateSchools->the_post();
+                    <? endwhile; wp_reset_postdata(); ?>
                     
-                            $promo = CFS()->get('on_promo');                    
-                            $school_type = get_the_tags();
-                            $schoolType = $school_type[0]->name;
-                            $content = get_the_excerpt();
-                            
-
-
-                        if($promo == true) { ?>
-                            
-                            <div class="school-div" style="background: url(<? echo get_the_post_thumbnail_url(); ?>) no-repeat center;">
-                                <div class="school-ovrly public"></div>
-                                <h2 class="school-title title"><? the_title(); ?></h2>
-                                <p class="school-type"><? echo $schoolType; ?></p>
-                                <div class="school-content">
-                                    <? echo $content; ?>
-                                    <a class="school-link" href=<? echo get_the_permalink(); ?>><? _e('Learn More', 'nexus'); ?></a>
-                                </div>
-                            </div>
-
-                        <? } ?>
-                    <? endwhile; ?>
-                    <?
-                        $args = array(
-                            'post_type' => 'schools',
-                            'posts_per_page' => 1,
-                            'order' => 'ASC',
-                            'tag' => 'language'
-                        ); 
-
-                        $privateSchools = new WP_query($args);
-
-                        while($privateSchools->have_posts() ) : $privateSchools->the_post();
-                    
-                            $promo = CFS()->get('on_promo');                    
-                            $school_type = get_the_tags();
-                            $schoolType = $school_type[0]->name;
-                            $content = get_the_excerpt();
-                            
-
-
-                        if($promo == true) { ?>
-
-                            
-                            <div class="school-div" style="background: url(<? echo get_the_post_thumbnail_url(); ?>) no-repeat center;">
-                                <div class="school-ovrly esl"></div>
-                                <h2 class="school-title title"><? the_title(); ?></h2>
-                                <p class="school-type"><? echo $schoolType; ?></p>
-                                <div class="school-content">
-                                    <? echo $content; ?>
-                                    <a class="school-link" href=<? echo get_the_permalink(); ?>><? _e('Learn More', 'nexus'); ?></a>
-                                </div>
-                            </div>
-
-                        <? } ?>
-                    <? endwhile; 
-                        wp_reset_postdata(); 
-                    ?>
-                </div>
 
                 <div class="promotions-desc"><? the_content(); ?></div>
 
