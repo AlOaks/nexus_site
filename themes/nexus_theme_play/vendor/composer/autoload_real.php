d31ff4cf51fedcbb7085c97060b7c4ef0a4f13a8
Al Oaks <jalberto.robles@outlook.com>
2019-04-17 12:51:35 -0700

    Mail

--- themes/nexus_theme_play/vendor/composer/autoload_real.php
+++ themes/nexus_theme_play/vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit3848efc8c13d8fdd26a056cb65cb2fb6
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit3848efc8c13d8fdd26a056cb65cb2fb6::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire3848efc8c13d8fdd26a056cb65cb2fb6($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire3848efc8c13d8fdd26a056cb65cb2fb6($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
