98ef925d4f9e49dc0d9ea1b87c093c1307a7c235
Al Oaks <jalberto.robles@outlook.com>
2019-05-22 09:37:34 -0700

    Front page

--- themes/nexus_theme_play/front-page.php
+++ themes/nexus_theme_play/front-page.php
@@ -89,38 +89,21 @@ $lang = get_locale();
 						<?php } ?>
 					</div>
 					<div id="he-hero-div-triangle"></div>
-				</div>
-				
-			</section>
-			
+				</div>	
+			</section>	
 			<section class="front-page-description">
-				<p>
-					<?php echo get_the_content(); ?>
-				</p>
+				<p><?php echo get_the_content(); ?></p>
 			</section>
 			<section class="front-page-blogpost-container">
-				<?php 
-					$args = array('p' => 492);
-
-					$newNexus = new WP_query($args);
-
-					while($newNexus->have_posts()) : $newNexus->the_post(); 
-				?>
-				<img class="post-thumbnail" src="<?php echo get_template_directory_uri().'/assets/images/Nexus-Evolution-3.png'; ?>" />
+				<img class="post-thumbnail" src="<?php echo CFS()->get('front-image'); ?>" />
 				<div class="post-data">
 					<h3><?php _e('Meet the new Nexus!', 'Front-Page'); ?></h3>
 					<div>
-						<p><?php _e('Nexus was created in 2009 and has always had the purpose of promoting the culture of exchanges in Brazil. After a few years, we have been led to understand our audience more deeply, to broaden our relationship, and to seek new ways to offer the best educational experience in Canada.', 'New Nexus'); ?></p>
-						<p><?php _e('Over the time we have identified that Nexus can go beyond helping people with the dream of doing a cultural exchange. We believe that we can make an even greater impact in contributing to building a better world by providing a universe of possibilities for each of our clients who want to build a new life in Canada.', 'New Nexus'); ?></p>
-						<p><em><?php _e('We live in constant evolution, transformation, and renewal. And the moment has come to tell you why it’s time for a new Nexus.', 'New Nexus'); ?></em></p>
+						<p><?php echo CFS()->get('front-content'); ?></p>	
 					</div>
 				</div>
-				<?php endwhile; ?>
 				<a href="#" class="get-quote-btn menu-item-51 menu-item-25"><?php _e('Chat with us!', 'Front-page'); ?></a>
 			</section>
-
-
-
 			<section class="why-nexus-section">
 				<h2 class="why-title"><?php _e('Why hire Nexus?', 'nexus'); ?></h2>
 				<div class="why-items-container">
