af429e95cd92eb26c52f659f886e5276fe4eb9ad
Al Oaks <jalberto.robles@outlook.com>
2019-06-17 10:13:28 -0700

    Contact

--- themes/nexus_theme_play/page-contactthanks.php
+++ themes/nexus_theme_play/page-contactthanks.php
@@ -15,8 +15,6 @@ session_start();
 
 get_header();
 
-$verified = tokenVer("contactform");
-
 require("vendor/autoload.php");
 
 use \DrewM\MailChimp\MailChimp;
@@ -25,63 +23,56 @@ $mailC = new MailChimp('c5d0f074ef62e6395640869899d38579-us8');
 
 $listID = '7dc11a23ab';
 
-if($verified === true) {
+    // Building a whitelist array with keys which will send through the form, no others would be accepted later on
+$allowedAccomFields = array('tokenField','nexus-email','contact-name', 'contact-email', 'contact-message', 'privacy-policy');
 
-		// Building a whitelist array with keys which will send through the form, no others would be accepted later on
-	$allowedAccomFields = array('tokenField','nexus-email','contact-name', 'contact-email', 'contact-message', 'privacy-policy');
-
-	// Building an array with the $_POST-superglobal 
-	foreach ($_POST as $key=>$item) {
-			
-	// Check if the value $key (fieldname from $_POST) can be found in the whitelisting array, if not, die with a short message to the hacker
-        if (!in_array($key, $allowedAccomFields)) {
-            
-            secureLog('Unknown form fields @ Contact Form');
-            die("Error detected. Please use only the fields in the form");
-            // header("refresh:1;url=https://nexuseducanada.com");
-            }
-    }
-
-        $name = stripcleantohtml($_POST['contact-name']);
-        $email = stripcleantohtml($_POST['contact-email']);
-        $message = stripcleantohtml($_POST['contact-message']);
-        $consent = stripcleantohtml($_POST['privacy-policy']);
-
-        $to = $_POST['nexus-email'];
-        $subject = 'Contact Form from'.$name;
-        $msg = '
-
-        <h1 style="color: red;">Contact Form Submitted by '.$name.'</h1>
-
-            Name: '.$name.'<br>
-            Email: '.$email.'<br>
-            Message: '.$message.'<br>
-            Privacy Policy: '.$consent.'<br>
-            Language: '.$lang.'<br>
-
-        ';
+// Building an array with the $_POST-superglobal 
+foreach ($_POST as $key=>$item) {
         
-        $headers .= "Reply-to: ".$name."<".$email.">\r\n";
-        $headers .= "Return-path: Nexus Contact <contact@nexuseducanada.com>\r\n";
-        $headers .= "From: Nexus Contact Form\r\n";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-        // echo '<h1>'.$to, $name, $email, $message, $consent.'</h1>';
-        wp_mail($to, $subject, $msg, $headers);
-
-    $newMember = $mailC->post('lists/'.$listID.'/members', [
-        'email_address' => $email,
-        'merge_fields' => [
-            'FNAME' => $name
-        ],
-        'status' => 'subscribed'
-    ]);
-
-} else {
-    secureLog('Form Token @ Contact Form PHP');
-    header("refresh:1;url=https://nexuseducanada.com");
+// Check if the value $key (fieldname from $_POST) can be found in the whitelisting array, if not, die with a short message to the hacker
+    if (!in_array($key, $allowedAccomFields)) {
+        
+        secureLog('Unknown form fields @ Contact Form');
+        die("Error detected. Please use only the fields in the form");
+        // header("refresh:1;url=https://nexuseducanada.com");
+        }
 }
+
+    $name = stripcleantohtml($_POST['contact-name']);
+    $email = stripcleantohtml($_POST['contact-email']);
+    $message = stripcleantohtml($_POST['contact-message']);
+    $consent = stripcleantohtml($_POST['privacy-policy']);
+
+    $to = $_POST['nexus-email'];
+    $subject = 'Contact Form from'.$name;
+    $msg = '
+
+    <h1 style="color: red;">Contact Form Submitted by '.$name.'</h1>
+
+        Name: '.$name.'<br>
+        Email: '.$email.'<br>
+        Message: '.$message.'<br>
+        Privacy Policy: '.$consent.'<br>
+        Language: '.$lang.'<br>
+
+    ';
+    
+    $headers .= "Reply-to: ".$name."<".$email.">\r\n";
+    $headers .= "Return-path: Nexus Contact <contact@nexuseducanada.com>\r\n";
+    $headers .= "From: Nexus Contact Form\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    // echo '<h1>'.$to, $name, $email, $message, $consent.'</h1>';
+    wp_mail($to, $subject, $msg, $headers);
+
+$newMember = $mailC->post('lists/'.$listID.'/members', [
+    'email_address' => $email,
+    'merge_fields' => [
+        'FNAME' => $name
+    ],
+    'status' => 'subscribed'
+]);
 
 
 ?>
